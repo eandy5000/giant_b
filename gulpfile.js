60d9f6fb56edcbf63d753afb308c07801b62cff1
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-04-14 12:28:15 -0500

    gulp and file system working

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
+    
+    
+gulp.task('default', ['index','comboSass','scss','script','watch']);
+
+gulp.task('watch', function(){
+    gulp.watch('client/views/index.html',['index']);
+    gulp.watch('client/scripts/app.js',['script']);
+});
+
+
+gulp.task('index', function (){
+   return gulp.src('client/views/index.html')
+          .pipe(gulp.dest('server/public/views/')); 
+});
+
+gulp.task('script', function (){
+   return gulp.src('client/scripts/app.js')
+       //   .pipe(uglify())
+          .pipe(gulp.dest('server/public/scripts/')); 
+});
+
+gulp.task('comboSass', function(){
+   return gulp.src('client/styles/scss/*.scss')
+          .pipe(concat('style.scss'))
+          .pipe(gulp.dest('client/styles/')); 
+});
+
+gulp.task('scss', function () {
+   return sass('client/styles/style.scss')
+          .pipe(gulp.dest('server/public/styles/')); 
+});
